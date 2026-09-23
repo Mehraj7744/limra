62db21062b165d1b8884df38f26d00a8904f8c0b
Mehraj Pathan <mehrajpathan7744@gmail.com>
2026-09-23 13:24:05 +0530

    errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
+import { redirect } from "next/navigation";
 
-import HomePage from "./[locale]/page";
-
-export default function Home() {
-  return (
-    <main>
-      <HomePage />
-
-    </main>
-  );
+export default function Page() {
+  redirect("/en");
 }
